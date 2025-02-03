f293dba108d7b400223f055e5df01ecb70c95487
Luca Nerlich <luca.nerlich@googlemail.com>
2025-02-03 13:34:05 +0100

    Update package-lock.json for dependency version changes
    Updated `package-lock.json` to reflect new or updated dependencies. This ensures compatibility with the latest library versions and maintains project dependencies in sync. No changes were made to the primary source code.

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -2,6 +2,9 @@ export default ({env}) => ({
     auth: {
         secret: env('ADMIN_JWT_SECRET'),
     },
+    watchIgnoreFiles: [
+        '**/config/sync/**',
+    ],
     apiToken: {
         salt: env('API_TOKEN_SALT'),
     },
