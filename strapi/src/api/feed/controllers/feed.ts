a037f1c3dab729a580ec6a570bc912ff35bdaf37
Luca Nerlich <luca.nerlich@googlemail.com>
2025-02-03 17:48:13 +0100

    Add document-based permission checks for feed API
    Enhanced the findOne controller for the feed API to validate access using documentId and tokens, introducing public and restricted feed handling. Updated user-role permissions for both public and authenticated roles to reflect the changes.

--- strapi/src/api/feed/controllers/feed.ts
+++ strapi/src/api/feed/controllers/feed.ts
@@ -1,7 +1,39 @@
-/**
- * feed controller
- */
+import {factories} from '@strapi/strapi'
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController('api::feed.feed', ({strapi}) => ({
 
-export default factories.createCoreController('api::feed.feed');
+    // finds by documentId
+    async findOne(ctx) {
+        await this.validateQuery(ctx)
+        const url = new URL('http://localhost:1337' + ctx.request.url);
+        const urlSearchParams = new URLSearchParams(url.search);
+
+        const response = await strapi.documents('api::feed.feed').findOne({
+            // get document id from request url
+            documentId: url.pathname.split('/api/feeds/')[1],
+            populate: ['episodes', 'allowed_users'],
+        });
+
+        if (response.public) {
+            delete response.allowed_users;
+            return response;
+        }
+
+        if (!urlSearchParams.has('token')) {
+            return ctx.forbidden("You need to pass your token as a query parameter 'token' to access this feed.");
+        }
+
+        const isAllowed = response.allowed_users?.some(
+            (user) => {
+                return user.token === urlSearchParams.get('token');
+            }
+        );
+
+        if (!isAllowed) {
+            return ctx.forbidden("You do not have access to this feed.");
+        }
+
+        delete response.allowed_users;
+        return response;
+    }
+}));
