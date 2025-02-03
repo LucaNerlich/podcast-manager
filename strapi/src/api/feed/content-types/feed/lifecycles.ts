9245ee0bacfc42b112279a7299e27c64092c75c6
Luca Nerlich <luca.nerlich@googlemail.com>
2025-02-03 18:11:05 +0100

    Generate RSS skeleton during feed creation
    A default RSS structure is created and assigned to the feed's data during the `beforeCreate` lifecycle. This ensures that new feeds have a basic XML template pre-populated with initial values such as title, description, and language.

--- strapi/src/api/feed/content-types/feed/lifecycles.ts
+++ strapi/src/api/feed/content-types/feed/lifecycles.ts
@@ -1,7 +1,23 @@
 export default {
     beforeCreate(event) {
         console.log("beforeCreate");
-        const {data, where, select, populate} = event.params;
+        console.log("event.params.data", event.params.data);
+        const emptyRss = `
+        <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+            <channel>
+                <title>${event.params.data.title}</title>
+                <description>${event.params.data.description}</description>
+                <language>de</language>
+                <copyright>All rights reserved</copyright>
+                <itunes:category text="Leisure"/>
+                <itunes:owner>
+                    <itunes:name>TODO</itunes:name>
+                    <itunes:email>TODO</itunes:email>
+                </itunes:owner>
+            </channel>
+        </rss>
+        `
+        event.params.data.data = emptyRss;
     },
 
     afterCreate(event) {
