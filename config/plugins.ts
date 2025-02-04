708a8f79e434b845eee417f7ca99019dd16704d2
Luca Nerlich <luca.nerlich@gmail.com>
2025-02-04 18:48:31 +0100

    Add AWS S3 upload provider configuration for Strapi
    Integrated AWS S3 as the file upload provider in Strapi by adding the `@strapi/provider-upload-aws-s3` package and configuring it in `plugins.ts`. The configuration supports customizable bucket selection based on environment variables. Updated `package.json` and `package-lock.json` to include necessary dependencies.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,37 @@
-export default () => ({});
+// return boolean from string
+function getBoolean(value) {
+    switch (value) {
+        case true:
+        case 'true':
+        case 1:
+        case '1':
+        case 'on':
+        case 'yes':
+            return true
+        default:
+            return false
+    }
+}
+
+const {env} = require('@strapi/utils')
+
+export default () => ({
+    upload: {
+        enabled: true,
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                        secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    },
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: getBoolean(env('USE_TEST_BUCKET')) ? env('AWS_BUCKET_TEST') : env('AWS_BUCKET'),
+                    },
+                },
+            },
+        },
+    },
+});
