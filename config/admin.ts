21c3d57c010ae751dc8575a5e38b686466739703
Luca Nerlich <luca.nerlich@googlemail.com>
2025-02-05 10:30:51 +0100

    Update environment defaults and middleware security config
    Add default values for admin JWT secret, API token salt, and transfer token salt to improve fallback handling. Enhance security middleware with a stricter Content Security Policy configuration to control script and image sources.

--- config/admin.ts
+++ config/admin.ts
@@ -1,16 +1,16 @@
 export default ({env}) => ({
     auth: {
-        secret: env('ADMIN_JWT_SECRET'),
+        secret: env('ADMIN_JWT_SECRET', '47e7ea9786c3576e3c3ee7632h4ddd0a'),
     },
     watchIgnoreFiles: [
         '**/config/sync/**',
     ],
     apiToken: {
-        salt: env('API_TOKEN_SALT'),
+        salt: env('API_TOKEN_SALT', '581f377575a900ea9bd3f9b68a947c9a'),
     },
     transfer: {
         token: {
-            salt: env('TRANSFER_TOKEN_SALT'),
+            salt: env('TRANSFER_TOKEN_SALT', 'abgCaErxoNdy4e8YFrHRabgCaEaxoNdy4e8YFrHR'),
         },
     },
     flags: {
--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,17 @@
 export default [
     'strapi::logger',
     'strapi::errors',
-    'strapi::security',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                directives: {
+                    'script-src': ['\'self\'', '\'unsafe-inline\''],
+                    'img-src': ['blob:', '\'self\'', 'data:', 'market-assets.strapi.io', 'strapi.io', 'strapi-cffc-test.s3.eu-central-1.amazonaws.com', 'strapi-cffc.s3.eu-central-1.amazonaws.com', 'dl.airtable.com'],
+                },
+            },
+        },
+    },
     'strapi::cors',
     'strapi::poweredBy',
     'strapi::query',
