455dcc63b3e63e05d852ec77db330244b9efd99e
Luca Nerlich <luca.nerlich@googlemail.com>
2025-02-05 16:12:54 +0100

    Make 'cover' field required and update type definitions
    Updated the 'cover' field in feed and episode schemas to be required. Added new properties (description, slug, public) to the Feed type definition for better clarity and usage. Minor formatting fixes in the episode schema for consistency.

--- src/utils/types/Feed.ts
+++ src/utils/types/Feed.ts
@@ -4,10 +4,13 @@ export interface Feed {
     id: number;
     documentId: string;
     title: string;
+    description: string;
+    slug: string;
+    public: boolean;
     createdAt: string;
     updatedAt: string;
     publishedAt?: string;
-    data: string;
+    data: string; // cache / the actual rss feed xml as a string
     email: string;
     copyright: string;
     owner: string;
