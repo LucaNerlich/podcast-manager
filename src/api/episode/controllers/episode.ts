b7043e83757eeb5964767350f2a76fbcc43ae37a
Luca Nerlich <luca.nerlich@gmail.com>
2025-11-22 12:53:04 +0100

    remove mp3 from token in controller

--- src/api/episode/controllers/episode.ts
+++ src/api/episode/controllers/episode.ts
@@ -5,13 +5,14 @@ export default factories.createCoreController('api::episode.episode', ({strapi})
     async fetch(ctx) {
         const {guid} = ctx.params;
         // set in feed controller -> processXmlWithToken
-        const {token} = ctx.query;
+        const {token} = ctx.query as { token?: string };
+        const normalizedToken = token.replace(/\.mp3$/i, '');
 
         if (!guid) {
             return ctx.badRequest('GUID parameter is missing');
         }
 
-        const episode = await strapi.service('api::episode.episode').findOne(guid, token);
+        const episode = await strapi.service('api::episode.episode').findOne(guid, normalizedToken);
 
         // Track download with Umami
         track("episode-fetch", episode?.title, guid)
@@ -27,13 +28,14 @@ export default factories.createCoreController('api::episode.episode', ({strapi})
     async download(ctx) {
         const {guid} = ctx.params;
         // set in feed controller -> processXmlWithToken
-        const {token} = ctx.query;
+        const {token} = ctx.query as { token?: string };
+        const normalizedToken = token.replace(/\.mp3$/i, '');
 
         if (!guid) {
             return ctx.badRequest('GUID parameter is missing');
         }
 
-        const episode = await strapi.service('api::episode.episode').findOne(guid, token);
+        const episode = await strapi.service('api::episode.episode').findOne(guid, normalizedToken);
 
         if (!episode) {
             return ctx.notFound();
